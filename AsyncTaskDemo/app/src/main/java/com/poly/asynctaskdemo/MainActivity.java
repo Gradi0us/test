1a3e7ad920c175d885cd2a6fe6da8c70bbe23c3d
Gradi0us <hieunguyen20032302@gmail.com>
2022-10-31 16:41:54 +0700

    first commit

---
+++ AsyncTaskDemo/app/src/main/java/com/poly/asynctaskdemo/MainActivity.java
@@ -0,0 +1,69 @@
+package com.poly.asynctaskdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv1);
+        new ReadRss().execute("https://vnexpress.net/rss/tin-moi-nhat.rss");
+    }
+
+    private class ReadRss extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder content = new StringBuilder();
+            try {
+                URL url = new URL(strings[0]);
+
+                InputStreamReader inputStreamReader = new InputStreamReader(url.openConnection().getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    content.append(line);
+                }
+                bufferedReader.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            XMLDOMParser parser = new XMLDOMParser();
+            Document document = parser.getDocument(s);
+            NodeList nodeList = document.getElementsByTagName("item");
+            String tieuDe = "";
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Element element = (Element) nodeList.item(i);
+                tieuDe += parser.getValue(element, "title") + "\n";
+            }
+            Toast.makeText(MainActivity.this, tieuDe, Toast.LENGTH_SHORT).show();
+            textView.setText(""+tieuDe);
+
+        }
+    }
+}
